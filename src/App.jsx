51bda60712865e2e0a308e042c9b795eedb18a7e
Tobias Thy <thymusiker@gmail.com>
2022-04-21 13:30:43 +0200

    Comments in BusTimes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.scss';
+import {BusTimes} from './components/bus/bus'
 
 function App() {
   return (
     <div className="App">
+      <BusTimes />
     </div>
   );
 }
--- src/components/bus/bus.jsx
+++ src/components/bus/bus.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react"
 import  axios from "axios"
 
+//exportere funktionen, så den kan hentes i vores app.jsx
 export const BusTimes = () => {
     const [ busPlan , setBusPlan ] = useState([]);
 
+    // Arrowfunction som henter info'en fra API'en
     useEffect(() => {
         const getData = async () => {
             const busApi = 'https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=8519734&format=json'
@@ -12,11 +14,14 @@ export const BusTimes = () => {
         }
         getData();
 
+        //Kører funktionen hvert 5. sekund, for at opdatere busplanen
         setInterval(() => {
             getData();
         }, 5000)
+        // Lytter efter ændringer i setBusPlan og renderer igen hvis den finder ændringer
     }, [setBusPlan]);
 
+    //Henter de værdier som er i de hentede objects, og indsætter dem i HTML elementer
     return(
         <section className="busPlanWrapper">
             <h2>Bus Plan</h2>
@@ -24,6 +29,7 @@ export const BusTimes = () => {
                 return(
                     <section key={index} className="busPlan">
                         <p>{plan.line}</p>
+                        <p>Fra: {plan.stop}</p>
                         <p>{plan.direction}</p>
                         <p className="time">{plan.time}</p>
                     </section>
