70410a1f3a469fe7248ba04b94a4d80058104e48
MariaEhlert <83949695+MariaEhlert@users.noreply.github.com>
2022-04-27 10:10:51 +0200

    Co-authored-by: TordisLaksa <TordisLaksa@users.noreply.github.com> Co-authored-by: Patrik Dahl <ohPatPat@users.noreply.github.com>

--- src/components/Nav/NavButton.jsx
+++ src/components/Nav/NavButton.jsx
@@ -1,6 +1,8 @@
 const NavButton = ({Url,clickEvent}) => {
     return (
-        <button id="MenuButton" onClick={() => clickEvent(true)}><div id="ButtonColor"><img src={Url} alt="" /></div></button>
+        <footer>
+            <button id="MenuButton" onClick={() => clickEvent(true)}><div id="ButtonColor"><img src={Url} alt="" /></div></button>
+        </footer>
             )
     }
     export default NavButton;
